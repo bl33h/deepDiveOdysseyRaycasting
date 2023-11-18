b40ee9be51a8ac18856a0229fecd108df7ebd225
bl33h <per21371@uvg.edu.gt>
2023-11-17 18:44:02 -0600

    assets placed correctly

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ Copyright (C), 2022-2023, Sara Echeverria (bl33h)
 FileName: main.cpp
 @version: I
 Creation: 06/11/2023
-Last modification: 17/11/2023
+Last modification: 17/11/2023 
 *Some parts were made using the AIs Bard and ChatGPT
 ------------------------------------------------------------------------------*/
 #include "resourceManager.h"
