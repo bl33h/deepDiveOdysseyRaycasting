06cd21bb606059fe9c0faa4c582312f79e659a7e
bl33h <per21371@uvg.edu.gt>
2023-11-17 18:40:10 -0600

    initial setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*---------------------------------------------------------------------------
 Copyright (C), 2022-2023, Sara Echeverria (bl33h)
 @author Sara Echeverria
@@ -10,8 +9,10 @@ Last modification: 09/11/2023
 ------------------------------------------------------------------------------*/
 
 #include "colors.h"
-#include "media.h"
+#include "resourceManager.h"
 #include "raycaster.h"
+#include "constants.h"
+#include "levels.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mixer.h>
 #include <SDL2/SDL_ttf.h>
@@ -19,25 +20,7 @@ Last modification: 09/11/2023
 #include <SDL_render.h>
 #include <SDL_video.h>
 
-=======
-/*---------------------------------------------------------------------------
-Copyright (C), 2022-2023, Sara Echeverria (bl33h)
-@author Sara Echeverria
-FileName: main.cpp
-@version: I
-Creation: 06/11/2023
-Last modification: 09/11/2023
-*Some parts were made using the AIs Bard and ChatGPT
-------------------------------------------------------------------------------*/
-
-#include "colors.h"
-#include "media.h"
-#include "raycaster.h"
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL_events.h>
-#include <SDL_render.h>
-#include <SDL_video.h>
-
->>>>>>> 1dba9dbf1e55a02149d6f582008c43b6bf1e0000
+// SDL window initial setup
+bool winner = false;
+SDL_Window* window;
+SDL_Renderer* renderer;
--- src/resourceManager.h
+++ src/resourceManager.h
@@ -8,13 +8,7 @@ Last modification: 09/11/2023
 *Some parts were made using the AIs Bard and ChatGPT
 ------------------------------------------------------------------------------*/
 
-#pragma once
-#include "colors.h"
-
-struct Vertex {
-  float x;
-  float y;
-  Color color;
-
-  Vertex(float xCoord, float yCoord, const Color& c) : x(xCoord), y(yCoord), color(c) {}
-};
+// Sound variables
+Mix_Chunk* walkForwardSound = nullptr;
+Mix_Chunk* bubbleSound = nullptr;
+Mix_Chunk* winnerSound = nullptr;
