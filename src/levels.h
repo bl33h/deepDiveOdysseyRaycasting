9e0db7517d2183e69826fd4e7213f763a4abdb25
bl33h <per21371@uvg.edu.gt>
2023-11-19 23:21:33 -0600

    documentation made

--- src/levels.h
+++ src/levels.h
@@ -17,11 +17,13 @@ Last modification: 17/11/2023
 #include <SDL_render.h>
 #include <SDL_video.h>
 
+// structure representing a game level
 struct Level {
-    std::string mapFile;
-    std::string name;
+    std::string mapFile; // file path of the level map (src/assets/levels)
+    std::string name; // name of the level
 };
 
+// vector containing predefined game levels
 std::vector<Level> levels = {
     { "src/assets/levels/beginner.txt", "Beginner" },
     { "src/assets/levels/intermediate.txt", "Intermediate" },
